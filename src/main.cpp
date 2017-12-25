f8845604bb761c8d944a77d116fbe0644159c10f
blueseazsc <bluesea.zhangsc@icloud.com>
2017-12-25 22:54:19 +0800

    add texture with repeat

--- src/main.cpp
+++ src/main.cpp
@@ -19,24 +19,30 @@ public:
 		glMatrixMode(GL_MODELVIEW);
 		glLoadIdentity();
 
-		texture = CreateTextureFromImage("/Users/zhangsc/Downloads/final/MyApp/Res/earth.bmp", true);
+		texture = CreateTextureFromImage("/Users/zhangsc/Downloads/final/MyApp/Res/head.png", true);
 	}
 	virtual void render(double currentTime)
 	{
-		glClearColor(0.f, 0.f, 0.f, 1.f);
+		glClearColor(0.2f, 0.4f, 0.6f, 1.f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
 		// draw triangle
 		glEnable(GL_TEXTURE_2D);
 		glBindTexture(GL_TEXTURE_2D, texture);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+		// glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 		glBegin(GL_TRIANGLES);
 		glColor4ub(255, 255, 255, 255); 
-		glTexCoord2f(0.f, 0.f);
-		glVertex3f(-0.2f, -0.2f, -2.5f);
-		glTexCoord2f(1.f, 0.f);
-		glVertex3f(0.2f, -0.2f, -2.5f);
-		glTexCoord2f(0.5f, 1.f);
-		glVertex3f(0.f, 0.2f, -2.5f);
+		glTexCoord2f(-1.f, 0.f);
+		glVertex3f(-0.2f, -0.2f, -0.5f);
+		glTexCoord2f(2.f, 0.f);
+		glVertex3f(0.2f, -0.2f, -0.5f);
+		glTexCoord2f(0.5f, 2.f);
+		glVertex3f(0.f, 0.2f, -0.5f);
 		glEnd();
 		glBindTexture(GL_TEXTURE_2D, 0);
 		glDisable(GL_TEXTURE_2D);
@@ -46,15 +52,15 @@ public:
 		glBegin(GL_LINES);
 		glColor4ub(255, 0, 0, 255); 
 		glVertex3f(0.f, 0.f, -2.5f);
-		glVertex3f(0.5f, 0.f, -2.5f);
+		glVertex3f(1.5f, 0.f, -2.5f);
 
 		glColor4ub(0, 255, 0, 255); 
 		glVertex3f(0.f, 0.f, -2.5f);
-		glVertex3f(0.f, 0.5f, -2.5f);
+		glVertex3f(0.f, 1.5f, -2.5f);
 
 		glColor4ub(0, 0, 255, 255); 
 		glVertex3f(0.f, 0.f, -2.5f);
-		glVertex3f(0.f, 0.f, -3.f);
+		glVertex3f(0.f, 0.f, -4.f);
 		glEnd();
 
 	}
